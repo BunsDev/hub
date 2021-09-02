cdfbca8137a17ae3185d578d06c95dac61b4a94f
Mike <mikelrap@gmail.com>
2021-09-02 17:37:49 +0300

    direct upload dropzone added

--- src/components/CreateApi/UploadBy/DirectUpload.tsx
+++ src/components/CreateApi/UploadBy/DirectUpload.tsx
@@ -2,11 +2,32 @@
 
 import NavButtons from '../NavButtons'
 import { Wrapper } from '../Wrapper'
+import Dropzone from 'react-dropzone'
 
 export const DirectUpload = () => {
   return (
     <Wrapper>
-      Direct Upload
+      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section
+            sx={{
+              width: '32.125rem',
+              height: '17.5rem',
+              border: '1.5px dashed #FFFFFF',
+              borderRadius: '1.25rem',
+              m: '0 auto',
+            }}
+          >
+            <div
+              {...getRootProps()}
+              sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+            >
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
       <NavButtons continueEnabled={true} />
     </Wrapper>
   )
