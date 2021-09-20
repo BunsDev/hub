83aee88966a3a89c4eb38706d26242f1cf421a83
cbrzn <cesarbrazon10@gmail.com>
2021-09-20 19:42:44 +0200

    chore: client fetch wrapper files

--- src/pages/api/ipfs/[hash]/pointers.ts
+++ src/pages/api/ipfs/[hash]/pointers.ts
@@ -4,7 +4,6 @@ import UriCacheRepository from "../../../../api/repositories/uriCacheRepository"
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getCustomRepository } from "typeorm";
 import { Web3ApiClient } from "@web3api/client-js";
-import { ensPlugin } from "@web3api/ens-plugin-js";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   if (request.method === "GET") {
@@ -14,24 +13,21 @@ export default async (request: VercelRequest, response: VercelResponse) => {
       const database = new Database();
       await database.connect();
 
-      const client = new Web3ApiClient({
-        plugins: [
-          {
-            uri: "w3://ens/ens.web3api.eth",
-            plugin: ensPlugin({}),
-          },
-        ],
-      });
+      const client = new Web3ApiClient();
 
-      const metadata = await client.getManifest("ens/yay2.open.web3api.eth", {
-        type: "meta",
-      });
+      const metadata = await client.getManifest(
+        "ens/ropsten/yay2.open.web3api.eth",
+        {
+          type: "meta",
+        }
+      );
 
       // TODO: get file
-      // client.getFile({
-      //   uri: uri,
-      //   path: metadata.icon,
-      // });
+      const t = await client.getFile("ens/ropsten/yay2.open.web3api.eth", {
+        path: metadata.queries[0].query,
+      });
+
+      console.log({ t });
 
       const uriCaches = await getCustomRepository(
         UriCacheRepository
